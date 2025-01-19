b8cc3631482e6340d3383b0d1655a0c8bda085b7
Zunaira Bakhtawar <zunairabakhtawar.dev@yahoo.com>
2025-01-18 21:42:16 -0800

    second program

--- pyton_basics_01/helloworld.py
+++ pyton_basics_01/helloworld.py
@@ -1,2 +1,13 @@
-# This is a basic Python program to print "Hello, World!"
-print("This removed data")
+# Program to add two numbers
+
+# Input: Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Input: Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Output: Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
