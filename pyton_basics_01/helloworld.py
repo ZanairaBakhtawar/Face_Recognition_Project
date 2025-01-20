c35d15d8ad4260f295d8a0d905efaefae687cef5
Zunaira Bakhtawar <zunairabakhtawar.dev@yahoo.com>
2025-01-19 16:33:27 -0800

    Update helloworld.py

--- pyton_basics_01/helloworld.py
+++ pyton_basics_01/helloworld.py
@@ -15,3 +15,10 @@ a =4
 b =5
 x =a+b
 print(x)
+price = 1000000
+has_good_credit = True
+
+if  has_good_credit:
+    down_payment = 0.1 * price
+else:down_payment =0.2 *price
+print(f"down payment:{down_payment}")
