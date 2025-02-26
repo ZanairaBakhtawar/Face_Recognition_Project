70f3e9b8339c780891dcc70ec04ec9e562001349
Zunaira Bakhtawar <zunairabakhtawar.dev@yahoo.com>
2025-02-26 10:37:25 -0800

    Update helloworld.py

--- pyton_basics_01/helloworld.py
+++ pyton_basics_01/helloworld.py
@@ -1,4 +1,27 @@
-class Student:  
-    name = "john"  
-s1 = Student()  
-print(s1.name)  
+class Dog:
+    def speak(self):
+        return "Woof!"
+
+class Cat:
+    def speak(self):
+        return "Meow!"
+
+# Using polymorphism
+animals = [Dog(), Cat()]
+
+for animal in animals:
+    print(animal.speak())
+
+
+
+class Bird:
+    def fly(self):
+        return "Can fly"
+
+class Penguin:
+    def fly(self):
+        return "Cannot fly"
+
+# Polymorphism in action
+for animal in [Bird(), Penguin()]:
+    print(animal.fly())
