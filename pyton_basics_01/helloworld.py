fdb22f34a70446a9f1b902340fe4893141e90d92
Zunaira Bakhtawar <zunairabakhtawar.dev@yahoo.com>
2025-02-27 10:27:20 -0800

    Update helloworld.py

--- pyton_basics_01/helloworld.py
+++ pyton_basics_01/helloworld.py
@@ -1,46 +1,9 @@
-class Dog:
-    def speak(self):
-        return "Woof!"
 
-class Cat:
-    def speak(self):
-        return "Meow!"
-
-# Using polymorphism
-animals = [Dog(), Cat()]
-
-for animal in animals:
-    print(animal.speak())
+import math
+x = math.sqrt(25)
+print(x) 
 
 
-
-class Bird:
-    def fly(self):
-        return "Can fly"
-
-class Penguin:
-    def fly(self):
-        return "Cannot fly"
-
-# Polymorphism in action
-for animal in [Bird(), Penguin()]:
-    print(animal.fly())
-
-
-class Car:
-    def fuel(self):
-        return "Petrol or Diesel"
-
-class ElectricCar:
-    def fuel(self):
-        return "Electricity"
-
-
-for vehicle in [Car(), ElectricCar()]:
-    print(vehicle.fuel())
-
-
-
-print(len("Hello"))  
-print(len([1, 2, 3, 4])) 
-print(len({"a": 1, "b": 2}))  
+import math
+x = math.pow(3,2)
+print(x)
