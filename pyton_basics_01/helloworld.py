b46774b6045cde95c1edf46d0eed2c82d2f3a82f
Zunaira Bakhtawar <zunairabakhtawar.dev@yahoo.com>
2025-03-01 11:46:08 -0800

    Update helloworld.py

--- pyton_basics_01/helloworld.py
+++ pyton_basics_01/helloworld.py
@@ -18,3 +18,22 @@ print(f"Factorial of {num} is {factorial}")
 import math 
 x= math.pi
 print(x)
+
+
+add = lambda x, y: x + y
+print(add(5, 3))  
+
+numbers = [1, 2, 3, 4]
+squared = list(map(lambda x: x ** 2, numbers))
+print(squared) 
+
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  
+
+
+students = [("Alice", 25), ("Bob", 22), ("Charlie", 23)]
+sorted_students = sorted(students, key=lambda x: x[1])  
+print(sorted_students)
+
+
